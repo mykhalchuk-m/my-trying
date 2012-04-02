d2739e14771762efa973d6de40630973fceb5ea5
mykhalchuk.m@gmail.com <mykhalchuk.m@gmail.com@ac5522a4-9422-63c2-18e8-81ebd039e897>
2012-04-02 07:21:06 +0000

    Initial import.

---
+++ spring-soc-sec/src/main/java/com/mmm/spring/socsec/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.mmm.spring.socsec.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:com/mmm/spring/socsec/config/security.xml")
+public class SecurityConfig {
+	
+}
---
+++ spring-soc-sec/src/main/java/com/mmm/spring/socsec/service/impl/UserServiseImpl.java
@@ -0,0 +1,31 @@
+package com.mmm.spring.socsec.service.impl;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mmm.spring.socsec.entity.User;
+import com.mmm.spring.socsec.service.UserService;
+
+@Repository
+public class UserServiseImpl implements UserService {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public User getUserById(String userId) {
+		User user = (User) sessionFactory.openSession()
+				.createCriteria(User.class)
+				.add(Restrictions.eq("userId", userId)).uniqueResult();
+		return user;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<User> getAllUsers() {
+		return (List<User>)sessionFactory.openSession().createCriteria(User.class).list();
+	}
+
+}
